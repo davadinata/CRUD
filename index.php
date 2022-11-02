6964c698ee41df6529bbb47b592ffe1c7a7ff954
Theodoxa <theodoxa.dava@gmail.com>
2022-11-02 15:27:39 +0700

    Tambahin nama dava di index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas 5 - Pemrograman Berbasis Platform</title>
+    <title>Tugas 5 [DAVA] - Pemrograman Berbasis Platform</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
